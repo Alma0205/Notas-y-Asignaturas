2821a7063b5daf974cb673ca21993a483ee8bbbd
Alma0205 <almacrespa10@gmail.com>
2025-08-18 19:03:59 -0500

    register sin conectar

---
+++ informacion.js
@@ -0,0 +1,9 @@
+let usuario = "";
+let clave = "";
+
+function registrar() {
+  usuario = prompt("Escribe tu usuario:");
+  clave = prompt("Escribe tu contraseña:");
+
+  alert(" Usuario registrado ");
+}
